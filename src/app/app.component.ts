05565f0c158572decaedb6a8495818be0423b58d
Noubissie Pavel <pavel.donfack@kaeyros-analytics.com>
2025-04-15 08:07:40 +0200

    feat(blog)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {HeaderComponent} from './shared/component/header/header.component';
 import {FooterComponent} from './shared/component/footer/footer.component';
 import {BodyComponent} from './shared/component/body/body.component';
-import {NgOptimizedImage} from '@angular/common';
+import {NgOptimizedImage, NgStyle} from '@angular/common';
 import {ButtonComponent} from './shared/component/button/button.component';
 
 @Component({
@@ -12,10 +12,69 @@ import {ButtonComponent} from './shared/component/button/button.component';
     FooterComponent,
     BodyComponent,
     NgOptimizedImage,
-    ButtonComponent
+    ButtonComponent,
+    NgStyle
   ],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+
+
+  public frontendSkills = [
+    {title: 'HTML / CSS', progress: 95},
+    {title: 'JavaScript / TypeScript', progress: 90},
+    {title: 'Angular / React', progress: 85}
+  ];
+
+  public backendSkills = [
+    {title: 'Node.js / Express', progress: 88},
+    {title: 'NestJS', progress: 80},
+    {title: 'MongoDB / SQL', progress: 75}
+  ];
+
+  public blogPosts = [
+    {
+      image: '/images/pixel.jpg',
+      title: 'Mastering Angular',
+      description: 'A journey through modern Angular development techniques.',
+      authorImage: '/images/pixel.jpg',
+      author: 'Jane Doe'
+    },
+    {
+      image: '/images/pixel.jpg',
+      title: 'Design Trends 2025',
+      description: 'Stay updated with the freshest UI/UX trends.',
+      authorImage: '/images/pixel.jpg',
+      author: 'John Smith'
+    },
+    {
+      image: '/images/pixel.jpg',
+      title: 'Design Trends 2025',
+      description: 'Stay updated with the freshest UI/UX trends.',
+      authorImage: '/images/pixel.jpg',
+      author: 'John Smith'
+    },
+    {
+      image: '/images/pixel.jpg',
+      title: 'Mastering Angular',
+      description: 'A journey through modern Angular development techniques.',
+      authorImage: '/images/pixel.jpg',
+      author: 'Jane Doe'
+    },
+    {
+      image: '/images/pixel.jpg',
+      title: 'Design Trends 2025',
+      description: 'Stay updated with the freshest UI/UX trends.',
+      authorImage: '/images/pixel.jpg',
+      author: 'John Smith'
+    },
+    {
+      image: '/images/pixel.jpg',
+      title: 'Design Trends 2025',
+      description: 'Stay updated with the freshest UI/UX trends.',
+      authorImage: '/images/pixel.jpg',
+      author: 'John Smith'
+    }
+  ];
 
 }
